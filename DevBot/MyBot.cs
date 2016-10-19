b8cbac92104f1223c92a26bf53d33160d048c4aa
Michael <Michael@TOWER>
2016-10-19 13:53:24 +0200

    Added Memes Added Answers Added Message Deletion
    Reorganized code a bit

--- DevBot/MyBot.cs
+++ DevBot/MyBot.cs
@@ -12,9 +12,35 @@ namespace DevBot
     class MyBot
     {
         DiscordClient discord;
+        CommandService commands;
+
+        Random randMemes;
+        Random randAnswers;
+
+        string[] allthememes;
+        string[] randomAnswers;
 
         public MyBot()
         {
+            randMemes = new Random();
+            randAnswers = new Random();    
+
+            allthememes = new string[]
+            {
+                ///Hier alle memes eintragen
+                "pictures/memes/datboi.gif",
+                "pictures/memes/dickbutt.png",
+                "pictures/memes/pepe_sad.jpg"
+            };
+
+            randomAnswers = new string[]  
+            {
+                ///Hier Random Antworten eintragen
+                "Hue",
+                "Heh",
+                "Ahaha",
+            };
+
             discord = new DiscordClient(x =>
             {
                 x.LogLevel = LogSeverity.Info;
@@ -23,22 +49,63 @@ namespace DevBot
 
             discord.UsingCommands(x =>
             {
-                x.PrefixChar = '#';                                                                                 ///Kommando Präfix (Was ich vor nem Kommando schreiben muss)
+                x.PrefixChar = '#';   ///Kommando Präfix (Was ich vor nem Kommando schreiben muss)
                 x.AllowMentionPrefix = true;
             });
 
-            var commands = discord.GetService<CommandService>();                                                    ///Kommandos
+            commands = discord.GetService<CommandService>();   ///Hiernach alle Befehle registrieren
 
-            commands.CreateCommand("hello")
-                .Do(async (e) =>
-                {
-                    await e.Channel.SendMessage("Hi");
-                });
+            RegisterDatboiCommand();
+            RegisterMemeCommand();
+            RegisterRandomAnswerCommand();
+            RegisterKillCommand();   ///Needs certain rights
 
             discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MjM4MjE5OTE4OTc2MzUyMjU4.CujDHw.EJlQ172xzhef16ExEMfCXZis_Ok",TokenType.Bot);   ///Bot Log-In Token
             });
+        }
+
+        private void RegisterDatboiCommand()   ///Datboi
+        {
+            commands.CreateCommand("meme datboi")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("pictures/memes/datboi.gif");
+                });
+        }
+
+        private void RegisterMemeCommand()   ///Random Meme incoming!
+        {
+            commands.CreateCommand("meme")
+                .Do(async (e) =>
+                {
+                    int randomMemeIndex = randMemes.Next(allthememes.Length);
+                    string memeToPost = allthememes[randomMemeIndex];
+                    await e.Channel.SendFile(memeToPost);
+                });
+        }
+
+        private void RegisterRandomAnswerCommand()   ///Random Answers incoming!
+        {
+            commands.CreateCommand("Antworte!")
+                .Do(async (e) =>
+                {
+                    int randomAnswerIndex = randAnswers.Next(randomAnswers.Length);
+                    string answerToPost = randomAnswers[randomAnswerIndex];
+                    await e.Channel.SendMessage(answerToPost);
+                });
+        }
+
+        private void RegisterKillCommand()   ///Delete all tha messages!
+        {
+            commands.CreateCommand("kill")
+                .Do(async (e) =>
+                {
+                    Message[] messagesToKill;
+                    messagesToKill = await e.Channel.DownloadMessages(100);
+                    await e.Channel.DeleteMessages(messagesToKill);
+                });
         }
 
         private void Log(object sender, LogMessageEventArgs e)
